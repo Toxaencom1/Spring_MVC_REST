9804c9532289b3143f495f171802ec88ecb8754e
Toxaencom1 <Toxaencom@gmail.com>
2023-11-19 15:49:44 +0600

    Add entity, and MVC logic, and Exception handling

---
+++ src/main/java/com/taxah/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,49 @@
+package com.taxah.spring.rest.controller;
+
+import com.taxah.spring.rest.entity.Employee;
+import com.taxah.spring.rest.exeption_handling.EmployeeIncorrectData;
+import com.taxah.spring.rest.exeption_handling.NoSuchEmployeeException;
+import com.taxah.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null){
+            throw new NoSuchEmployeeException("There is no Employee with ID = " + id + " in Database");
+        }
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/taxah/spring/rest/exeption_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.taxah.spring.rest.exeption_handling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
